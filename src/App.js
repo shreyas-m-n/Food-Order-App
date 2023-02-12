e47ff0b9b2db1510724d0f0d3833813eef064dea
shreyasmn <mnshreyasiyengar@gmail.com>
2023-02-13 00:46:32 +0530

    added modal and backdrop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import { Header } from "./components/Layout/Header";
 import { AvailableMeals } from "./components/Meals/AvailableMeals";
+import { Cart } from "./components/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
       <Header></Header>
       <main>
+        <Cart></Cart>
         <AvailableMeals></AvailableMeals>
       </main>
     </Fragment>
